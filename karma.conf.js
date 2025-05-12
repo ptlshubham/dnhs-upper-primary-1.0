8b6cb205ae0a87989738e1051c564eb7b21f6460
Shubham Patel <ptlshubham@gmail.com>
2025-05-12 21:49:05 +0530

    All change

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/svp-higher-secondary-school'),
+      dir: require('path').join(__dirname, './coverage/dnhs-upper-primary'),
       subdir: '.',
       reporters: [
         { type: 'html' },
